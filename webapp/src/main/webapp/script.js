7bf45dd6e41be25626aacfad8a780603850ac24b
mobanntechnologies <mobanntechnologies@gmail.com>
2024-11-16 18:52:19 +0000

    Update script.jss

--- webapp/src/main/webapp/script.js
+++ webapp/src/main/webapp/script.js
@@ -1,6 +1,6 @@
 function displayGreeting() {
     var name = document.getElementById('name').value;
     var greetingElement = document.getElementById('greeting');
-    greetingElement.textContent = 'Hello, ' + name + '! Welcome to the Personalized Greeting App.';
+    greetingElement.textContent = 'Hello, ' + name + '! Welcome to the Mobann DevOps App.';
 }
 
